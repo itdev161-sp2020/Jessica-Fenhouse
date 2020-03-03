e6b1eec09cd61367a63cbf12835f87519bb4c7fc
Jessica Fenhouse <Fenhousj@gmatc.matc.edu>
2020-03-03 10:35:48 -0600

    added comment

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ connectDatabase();
 app.get('/', (req, res) => 
 res.send('http get request sent to root api endpoint')
 );
+//comment
 
 //Connection listener
 app.listen(3000, () => console.log('Express server running on port 3000'));
