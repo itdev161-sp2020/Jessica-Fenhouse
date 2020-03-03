0df3acf50cc050607ad5e2914c3f82601e311187
Jessica Fenhouse <Fenhousj@gmatc.matc.edu>
2020-03-03 13:13:55 -0600

    Completed Activity 3

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectDatabase from './config/db';
+import { check, validationResult } from 'express-validator';
 
 //Initialize express application
 const app = express();
@@ -7,11 +8,29 @@ const app = express();
 //Connect database
 connectDatabase();
 
+//Configure Middleware
+app.use(express.json({extended: false}));
+
 //API endpoints
 app.get('/', (req, res) => 
 res.send('http get request sent to root api endpoint')
 );
-//comment
+
+app.post('/api/users',
+[
+    check('name', 'Please enter your name').not().isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+], 
+(req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    } else {
+        return res.send(req.body);
+    }
+});
+
 
 //Connection listener
 app.listen(3000, () => console.log('Express server running on port 3000'));
