eb933fcaa9bf77990ccb4b232ae04a7700b34aad
Jessica Fenhouse <Fenhousj@gmatc.matc.edu>
2020-05-05 15:30:58 -0500

    update to App.css and App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,7 @@ import Login from './components/Login/Login';
 
 class App extends React.Component {
   state = {
-    data: null,
+    posts: [],
     token: null,
     user: null,
   };
@@ -43,7 +43,15 @@ class App extends React.Component {
       axios.get("http://localhost:500/api/auth", config)
         .then((response) => {
           localStorage.setItem("user", response.data.name);
-          this.setState({ user: response.data.name });
+          this.setState(
+            {
+              user: response.data.name, 
+              token: token
+            },
+            () => {
+              this.loadData();
+            }
+          );
         })
         .catch((error) => {
           localStorage.removeItem("user");
@@ -53,6 +61,28 @@ class App extends React.Component {
     }
   }
 
+  loadData = () => {
+    const { token } = this.state;
+    
+    if (token) {
+      const config = {
+        headers: {
+          'x-auth-token': token
+        }
+      };
+      axios
+        .get('http://localhost:5000/api/posts', config)
+        .then(response => {
+          this.setState({
+            posts: response.data,
+          });
+        })
+        .catch(error => {
+          console.error("Error fetching data: ${error}");
+        });
+    }
+  };
+
   logOut = () => {
     localStorage.removeItem("token");
     localStorage.removeItem('user');
@@ -60,7 +90,7 @@ class App extends React.Component {
   }
 
   render() {
-    let { user, data } = this.state;
+    let { user, posts } = this.state;
     const authProps = {
       authenticateUser: this.authenticateUser
     }
@@ -87,16 +117,21 @@ class App extends React.Component {
           </header>
           <main>
             <Route exact path="/">
-              {user? 
+              {user? (
               <React.Fragment>
                 <div>Hello {user}!</div>
-              <div>{data}</div>
-              </React.Fragment>:
-              <React.Fragment>
-                Please Register or Login
+              <div>
+                {posts.map(post => (
+                  <div key={post.id}>
+                    <h1>{post.title}</h1>
+                <p>{post.body}</p>
+                  </div>
+                ))}
+              </div>
               </React.Fragment>
-              }
-              
+              ) : (
+              <React.Fragment>Please Register or Login</React.Fragment>
+              )}
             </Route>
             <Switch>
               <Route 
