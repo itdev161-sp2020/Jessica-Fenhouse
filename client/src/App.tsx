68215f8f7acc90e79482829c3da09a6610a40deb
Jessica Fenhouse <Fenhousj@gmatc.matc.edu>
2020-05-05 16:17:55 -0500

    updats to App.css and App.tsx and Adding Post/ and PostList/ folders

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,10 +4,13 @@ import axios from 'axios';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
+import PostList from './components/PostList/PostList';
+import Post from './components/Post/Post';
 
 class App extends React.Component {
   state = {
     posts: [],
+    post: null,
     token: null,
     user: null,
   };
@@ -83,6 +86,13 @@ class App extends React.Component {
     }
   };
 
+  viewPost = post => {
+    console.log('view ${post.title}');
+    this.setState({
+      post: post
+    });
+  };
+
   logOut = () => {
     localStorage.removeItem("token");
     localStorage.removeItem('user');
@@ -116,24 +126,20 @@ class App extends React.Component {
             </ul>
           </header>
           <main>
-            <Route exact path="/">
-              {user? (
-              <React.Fragment>
-                <div>Hello {user}!</div>
-              <div>
-                {posts.map(post => (
-                  <div key={post.id}>
-                    <h1>{post.title}</h1>
-                <p>{post.body}</p>
-                  </div>
-                ))}
-              </div>
-              </React.Fragment>
-              ) : (
-              <React.Fragment>Please Register or Login</React.Fragment>
-              )}
-            </Route>
             <Switch>
+              <Route exact path="/">
+                {user? (
+                <React.Fragment>
+                  <div>Hello {user}!</div>
+                  <PostList posts={posts} clickPost={this.viewPost} />
+              </React.Fragment>
+                ) : (
+                  <React.Fragment>Please Register or Login</React.Fragment>
+                )}
+              </Route>
+              <Route path="/posts/:postId">
+                <Post post={post} />
+              </Route>
               <Route 
               exact path="/register" 
               render={() => <Register {...authProps} />} />
